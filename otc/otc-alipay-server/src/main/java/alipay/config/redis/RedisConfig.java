ec39fdd0355c3436242a28e82961755213c975c1
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2022-07-14 21:19:56 +0800

    //银行编码从数据库中匹配

--- otc/otc-alipay-server/src/main/java/alipay/config/redis/RedisConfig.java
+++ otc/otc-alipay-server/src/main/java/alipay/config/redis/RedisConfig.java
@@ -11,9 +11,13 @@ import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
@@ -103,7 +107,13 @@ public class RedisConfig extends CachingConfigurerSupport {
         };
     }
 
-
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisTemplate redisTemplate) {
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisTemplate.getConnectionFactory());
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()));
+        return new CustomRedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+    }
     @Override
     @Bean
     public CacheErrorHandler errorHandler() {
