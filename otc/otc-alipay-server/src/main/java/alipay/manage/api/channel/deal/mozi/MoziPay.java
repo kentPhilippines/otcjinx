7c53bf6e1127ea2308f69108d281ec97cfa5f4e7
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2023-07-13 13:52:48 +0800

    //add mozi

---
+++ otc/otc-alipay-server/src/main/java/alipay/manage/api/channel/deal/mozi/MoziPay.java
@@ -0,0 +1,121 @@
+package alipay.manage.api.channel.deal.mozi;
+
+import alipay.manage.api.channel.util.ChannelInfo;
+import alipay.manage.api.channel.util.yifu.YiFuUtil;
+import alipay.manage.api.config.PayOrderService;
+import alipay.manage.bean.DealOrderApp;
+import alipay.manage.bean.UserInfo;
+import alipay.manage.bean.util.ResultDeal;
+import alipay.manage.service.UserInfoService;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import otc.common.PayApiConstant;
+import otc.result.Result;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component("MoziPay")
+public class MoziPay extends PayOrderService {
+    @Autowired
+    private UserInfoService userInfoServiceImpl;
+
+    @Override
+    public Result deal(DealOrderApp dealOrderApp, String channel) throws Exception {
+        log.info("【进入mozi，当前请求产品：{}，当前请求渠道：{} 】", JSONUtil.toJsonStr(dealOrderApp),channel);
+        String orderId = create(dealOrderApp, channel);
+        UserInfo userInfo = userInfoServiceImpl.findUserInfoByUserId(dealOrderApp.getOrderAccount());
+        log.info("userInfo:{}",JSONUtil.toJsonStr(userInfo));
+        if (StrUtil.isBlank(userInfo.getDealUrl())) {
+            orderEr(dealOrderApp, "当前商户交易url未设置");
+            return Result.buildFailMessage("请联系运营为您的商户好设置交易url");
+        }
+        ChannelInfo channelInfo = getChannelInfo(channel, dealOrderApp.getRetain1());
+        log.info("channel:{},",JSONUtil.toJsonStr(channelInfo));
+        Result result = createOrder(
+                userInfo.getDealUrl() +
+                        PayApiConstant.Notfiy.NOTFIY_API_WAI + "/moziPayNotify",
+                dealOrderApp.getOrderAmount(),
+                orderId,channelInfo
+                );
+        if (result.isSuccess()) {
+            return Result.buildSuccessResult("支付处理中", ResultDeal.sendUrl(result.getResult()));
+        } else {
+            orderEr(dealOrderApp, result.getMessage());
+            return result;
+        }
+    }
+
+    //{"msg":"success","code":200,"data":{"pc_pay_url":"http://api.55880.top/gateway/selfpay/paycode.do?id=4537308","qrcode_url":"http://api.55880.top/gateway/selfpay/paycode.do?id=4537308","qrcode":"http://api.55880.top/gateway/selfpay/paycode.do?id=4537308","trade_no":"4613820230628535252","sdk":"","order_id":"4537308","pay_url":"http://api.55880.top/gateway/selfpay/paycode.do?id=4537308"}}】
+    private Result createOrder(String notify, BigDecimal orderAmount,
+                               String orderId,
+                               ChannelInfo channelInfo) throws IOException {
+        String amount = orderAmount.intValue()+"";
+        String channel = channelInfo.getChannelType();
+        String merchantId = channelInfo.getChannelAppId();
+        String url = channelInfo.getDealurl();
+        //String key = "SCSD7lwUYyv2xVCU9grlV2ByuRtJPmv3LbiGcEF6SLUWsao8k7rc8Y8HtWYgVgs3";
+//        String key = channelInfo.getChannelPassword().split(",")[0];
+//        String token = channelInfo.getChannelPassword().split(",")[1];
+        //String token = "P7q2kLuGb0K0QQu7LGSkds7XG5Q0772m";
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("account_id", merchantId);
+        map.put("content_type", "json");
+        map.put("thoroughfare", channel);
+        map.put("out_trade_no",orderId);
+        map.put("amount",  amount);
+        map.put("callback_url", notify);
+        map.put("success_url", notify);
+        map.put("error_url", notify);
+        map.put("timestamp", DateTime.now().getTime());
+        map.put("ip", "127.0.0.1");
+        String createParam = YiFuUtil.createParam(map)+ "key="+channelInfo.getChannelPassword();
+        System.out.println("【mozi签名前参数："+createParam+"】");
+        String md5 = YiFuUtil.md5(createParam );
+        String sign = md5.toLowerCase();
+        map.put("sign", sign);
+
+        String reqUrl = url;
+        log.info("reqUrl：" + reqUrl);
+
+        String res = null;
+        try {
+            String post = HttpUtil.post(url, map);
+            log.info("请求结果：" + post);
+            if (StringUtils.isNotEmpty(post)) {
+                JSONObject jsonObject = JSONUtil.parseObj(post);
+                String code = jsonObject.getStr("code");
+                if ("200".equals(code)) {
+                    String resultUrl = JSONUtil.toBean(jsonObject.get("data")+"",Map.class).get("pay_url")+"" ;
+                    return Result.buildSuccessResult("支付处理中", resultUrl);
+                }
+            } else {
+                res = "操作失败";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            res = "系统异常";
+        }
+        return Result.buildFailMessage(res);
+        //return Result.buildSuccessResult("支付处理中", "payUrl");
+    }
+
+
+    public static void main(String[] args) {
+        String str="{\"code\":\"200\",\"status\":\"10000\",\"msg\":\"下单成功\",\"data\":{\"mch_id\":1105986,\"out_trade_no\":\"12018011512301500007\",\"total\":100,\"type\":\"9018\",\"timestamp\":1646580768,\"qr_url\":\"http:\\/\\/47.104.134.244\\/api\\/Cashier?UserId=1005986&CustomerOrderId=G100226-2203067054025&PayType=unicomalipay&Parvalue=100&Sign=947f2eaf90f2bbb9c7caeb4751d7851d\",\"h5_url\":\"http:\\/\\/47.104.134.244\\/api\\/Cashier?UserId=1005986&CustomerOrderId=G100226-2203067054025&PayType=unicomalipay&Parvalue=100&Sign=947f2eaf90f2bbb9c7caeb4751d7851d\"}}";
+        Map<String, Object> resultMap = JSONUtil.toBean(str,Map.class);
+        String resultUrl = JSONUtil.toBean(resultMap.get("data").toString(),Map.class).get("qr_url")+"" ;
+    }
+    //amount=1000.00&appId=jinsha888&applyDate=20220217152028&notifyUrl=http://34.96.135.66:5055/api-alipay&orderId=c4866ce376424932ae049ee3e8c2388b&pageUrl=http://34.96.135.66:5055/api-alipay&passCode=ALIPAYTOBANK&subject=1000.00&userid=王富贵
+
+}
---
+++ otc/otc-alipay-server/src/main/java/alipay/manage/api/channel/deal/mozi/MoziPayNotify.java
@@ -0,0 +1,64 @@
+package alipay.manage.api.channel.deal.mozi;
+
+import alipay.manage.api.channel.util.yifu.YiFuUtil;
+import alipay.manage.api.config.NotfiyChannel;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import otc.common.PayApiConstant;
+import otc.result.Result;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping(PayApiConstant.Notfiy.NOTFIY_API_WAI)
+public class MoziPayNotify extends NotfiyChannel {
+
+    /**
+     * 测试报文：{"merchantId":"200289","version":"1.0","orderNo":"J20220217182526265927355","amount":"5000","tradeDate":"20220217","tradeTime":"183528","resultCode":"0","sign":"d1487a8740f16b44ba3d0e1d42ff68a4"}
+     * {"error_code":10001,"error_msg":"不要重复下订单哦！","request_url":"\/api\/order"}
+     * @param request
+     * @param params
+     * @return
+     */
+    @RequestMapping(value = "/moziPayNotify")
+    @ResponseBody
+    public String chaofanPayNotify( HttpServletRequest request,@RequestParam Map<String,String> params) {
+        log.info("moziPayNotify notify:{}", JSONUtil.toJsonStr(params));
+//        String requestStr="{\"amount\":\"300.00\",\"body\":\"test\",\"channel\":\"alipayCodeSmall\",\"order_status\":\"1\",\"out_trade_no\":\"1671060097960382464\",\"trade_no\":\"1671060097960382464\",\"sign\":\"9442e429aae0db59c8001584dbbb5a5e\"}";
+//        params = JSONUtil.toBean(requestStr,Map.class);
+        String clientIP = HttpUtil.getClientIP(request);
+
+        String orderId = params.get("out_trade_no");
+//        String password = getChannelKey(orderId);
+//        this.getChannelKey("");
+        String postSign= params.get("sign");
+        String sign = createSign(params, "CE242C6F988ACD");
+        if(postSign.equalsIgnoreCase(sign) && "4".equals(params.get("pay_status")+"")) {
+            Result result = dealpayNotfiy(orderId+"", clientIP, "三方回调成功");
+            return "success";
+        }
+        return "error";
+    }
+
+
+    private static String createSign(Map map, String key)
+    {
+        map.remove("sign");
+        Map<String, String> params = map;
+//        params.put("key", key);
+//        StringBuilder buf = new StringBuilder((params.size() + 1) * 10);
+//        SignUtils.buildPayParams(buf, params, false);
+        String preStr = YiFuUtil.createParam(map)+"key="+key;
+        log.info("before sign：{}",preStr);
+        String sign = YiFuUtil.md5(preStr).toLowerCase();
+
+        return sign;
+    }
+}
