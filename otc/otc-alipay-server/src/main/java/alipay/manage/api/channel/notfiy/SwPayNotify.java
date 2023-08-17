14c3a6adb548dbded6606ccd36ba4b4807f82f2e
smith <jacksmithphi@gmail.com>
2023-08-17 22:46:03 +0800

    //sw

--- otc/otc-alipay-server/src/main/java/alipay/manage/api/channel/notfiy/SwPayNotify.java
+++ otc/otc-alipay-server/src/main/java/alipay/manage/api/channel/notfiy/SwPayNotify.java
@@ -56,7 +56,7 @@ public class SwPayNotify extends NotfiyChannel {
         String orderId =params.get("OrderNo");
         String sign =params.get("Sign");
         params.remove("Sign");
-        String channelKey = getChannelKey(orderId+"");
+        String channelKey = getDPAyChannelKey(orderId+"");
 
         String createParam = createParam(params);
         log.info("【sw代付签名前参数：" + createParam + "】");
