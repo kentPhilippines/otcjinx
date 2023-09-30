b597bfffb19c69e4aad2403ffda84c0aaf6c3467
kent.liu.philippines@gmail.com <liu947466968>
2023-09-30 20:59:11 +0800

    多处改动

--- otc/otc-alipay-server/src/main/java/alipay/manage/api/V2/OrderQueryApi1.java
+++ otc/otc-alipay-server/src/main/java/alipay/manage/api/V2/OrderQueryApi1.java
@@ -1,6 +1,6 @@
 package alipay.manage.api.V2;
 
-import alipay.manage.api.V2.vo.BalanceRequestVo;
+import alipay.manage.api.V2.vo.OrderRequestVo;
 import alipay.manage.api.deal.QueryV2Api;
 import cn.hutool.json.JSONUtil;
 import lombok.AllArgsConstructor;
@@ -16,29 +16,28 @@ import javax.validation.Valid;
 @RequestMapping("/v2/deal")
 @AllArgsConstructor
 /**
- * 账户查询
+ * 订单查询
  */
 @CrossOrigin
-public class BalanceQueryApi {
+public class OrderQueryApi1 {
     private QueryV2Api queryV2Api;
 
+
     /**
-     * 查询账户余额
+     * 查询订单状态
      *
-     * @param balanceRequestVo
+     * @param orderRequestVo
      * @return
      */
-    @PostMapping("/query/balence")
-    public Result balence(HttpServletRequest request, @RequestBody @Valid BalanceRequestVo balanceRequestVo) {
-        log.info("v2 balence request:{}", JSONUtil.toJsonStr(balanceRequestVo));
+    @PostMapping("/query/order")
+    public Result order(HttpServletRequest request, @RequestBody @Valid OrderRequestVo orderRequestVo) {
+        log.info("v2 order request:{}", JSONUtil.toJsonStr(orderRequestVo));
         try {
-            Result result = queryV2Api.queryBalance(balanceRequestVo);
-            log.info("v2 balence response:{}", JSONUtil.toJsonStr(result));
+            Result result = queryV2Api.queryOrder(orderRequestVo);
+            log.info("v2 order response:{}", JSONUtil.toJsonStr(result));
             return result;
-        }catch (Throwable e ){
+        } catch (Throwable e) {
             return Result.buildFailMessage("查询异常");
         }
     }
-
-
 }
